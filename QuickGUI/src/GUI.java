acd0815ed41dc5b762932be216319c8c8380d6c9
Nolan Trapp <officialstackemup@gmail.com>
2020-02-17 20:34:57 -0800

    Add files via upload

---
+++ QuickGUI/src/GUI.java
@@ -0,0 +1,59 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GUI implements ActionListener {
+	
+	int count = 0;
+	private JLabel label;
+	private JFrame frame;
+	private JPanel panel;
+	
+	public GUI() {
+		frame = new JFrame();
+		
+		JButton button = new JButton("Click Me");
+		label = new JLabel("Number of Clicks: 0");
+		button.addActionListener(this );
+		
+		
+		panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+		panel.setLayout(new GridLayout(0, 1));
+		panel.add(button);
+		panel.add(label);
+		
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("The Clicky Button");
+		frame.pack();
+		frame.setVisible(true);
+		
+//		JFrame.setIconImage(new ImageIcon(getClass().getClassLoader().getResource("Desktop/TNT_Revision_2")));
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new GUI();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		count++;
+		label.setText("Number of Clicks: " + count);
+		
+		
+	}
+
+}
